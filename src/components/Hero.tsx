3addcc02cf356cc24f9b331f28e46f45357ab377
neudataing <neudataing@gmail.com>
2025-07-09 15:27:20 +0000

    modified:   src/components/Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -42,7 +42,7 @@ const Hero: React.FC = () => {
               <img
                 src="/img/Logo-ES.png"
                 alt="Logo"
-                className="w-80 h-80 object-contain mx-auto"
+                className="w-90 h-90 object-contain mx-auto"
               />
               <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent"></div>
             </div>
