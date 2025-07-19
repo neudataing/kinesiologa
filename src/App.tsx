aa7c592f8037187e17f16ec837637ae2c54b79b7
neudataing <neudataing@gmail.com>
2025-07-19 13:48:20 +0000

    FAQ añadido

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Services2 from './components/Services2';
+import FAQ from './components/FAQ';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -17,6 +18,7 @@ function App() {
             <Hero />
             <About />
             <Services2 />
+            <FAQ />
             <Contact />
         </main>
         <Footer />
