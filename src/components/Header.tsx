fb5f68fbdea93b2316e40c6070c134944a64e8d6
neudataing <neudataing@gmail.com>
2025-07-19 16:13:14 +0000

    cambios

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -59,6 +59,16 @@ const Header: React.FC = () => {
               className="text-gray-800 text-base font-semibold hover:text-brand-500 transition-all duration-300 relative group"
             >
               Servicios
+
+              <button 
+              onClick={() => scrollToSection('FAQ')}
+              className="text-gray-800 text-base font-semibold hover:text-brand-500 transition-all duration-300 relative group"
+            >
+              Preguntas frecuentes
+              <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-brand-500 transition-all duration-300 group-hover:w-full"></span>
+            </button>
+
+
               <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-brand-500 transition-all duration-300 group-hover:w-full"></span>
             </button>
 
