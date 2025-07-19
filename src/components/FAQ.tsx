8097b7ed85a82a62a90123b4be7a77d713966b37
neudataing <neudataing@gmail.com>
2025-07-19 16:34:56 +0000

    modified:   src/components/FAQ.tsx

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -67,7 +67,7 @@ const FAQ: React.FC = () => {
 
 
   return (
-    <section className="py-20 bg-gray-50">
+    <section id="FAQ" className="py-20 bg-gray-50">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div 
           ref={ref}
