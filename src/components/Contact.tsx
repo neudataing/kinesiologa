ebdb139261892cfab6feaf67045c9459e38dcf07
neudataing <neudataing@gmail.com>
2025-07-20 21:59:58 +0000

    bcd

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,7 +9,7 @@ const Contact: React.FC = () => {
   });
 
   return (
-    <section id="contact" className="bg-blue-50 pt-12 pb-12">
+    <section id="contact" className="bg-blue-50 pt-12 pb-20">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
         <div 
           ref={ref}
