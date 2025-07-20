92c02bd1d5c2d9766394fe80734b913a3a0fcfa2
neudataing <neudataing@gmail.com>
2025-07-20 21:07:04 +0000

    aaaaa

--- src/components/Services2.tsx
+++ src/components/Services2.tsx
@@ -160,9 +160,9 @@ const Services: React.FC = () => {
 
   
         <div className="bg-white w-full">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-0">
             <div className="mt-8 lg:mt-12 bg-transparent">
-            <h2 className="pt-12 text-4xl lg:text-5xl font-bold text-gray-900 mb-6">
+            <h2 className="pt-12 text-center text-4xl lg:text-5xl font-bold text-gray-900 mb-6">
               Mi trabajo en <span className="text-brand-300">imágenes</span>
             </h2>
               <div className="max-w-6xl mx-auto">
